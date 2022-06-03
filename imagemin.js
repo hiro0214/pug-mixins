164b29eeed38f621976b2440de6caefdf76d8dd0
hiro0214 <leonhurt2@gmail.com>
2022-06-03 14:08:24 +0900

    add: picture mixins

--- imagemin.js
+++ imagemin.js
@@ -1,11 +1,17 @@
+import fs from 'fs';
+import glob from 'glob';
 import imageminGifsicle from 'imagemin-gifsicle';
 import imagemin from 'imagemin-keep-folder';
 import imageminMozjpeg from 'imagemin-mozjpeg';
 import imageminPngquant from 'imagemin-pngquant';
 import imageminSvgo from 'imagemin-svgo';
+import sharp from 'sharp';
+
+const inputDir = 'src/assets/images';
+const outputDir = 'dist/images/';
 
 const imageBuild = () => {
-  imagemin([`src/assets/images/**/*.{jpg,png,gif,svg,ico}`], {
+  imagemin([`${inputDir}/**/*.{jpg,png,gif,svg,ico}`], {
     plugins: [
       imageminMozjpeg({ quality: 80 }),
       imageminPngquant({ quality: [0.65, 0.8] }),
@@ -13,9 +19,26 @@ const imageBuild = () => {
       imageminSvgo(),
     ],
     replaceOutputDir: (output) => {
-      return output.replace(/images\//, `../../dist/images/`);
+      return output.replace(/images\//, `../../${outputDir}`);
     },
   });
 };
 
+const webpConvert = () => {
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+
+  const entries = glob.sync(`${inputDir}/**/*.{jpg,png}`);
+  for (let i = 0; i < entries.length; i++) {
+    const entry = entries[i];
+    const file = entry.split('/').slice(-1)[0];
+    const fileName = file.slice(0, -4);
+    sharp(entry)
+      .webp({ quality: 50 })
+      .toFile(`${outputDir + fileName}.webp`);
+  }
+};
+
 imageBuild();
+webpConvert();
